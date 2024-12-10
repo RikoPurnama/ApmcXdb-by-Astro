947f901724e3b3ee04e2de4d056b4f257b20169f
RikoPurnama <rikopurnama9051@gmail.com>
2024-12-11 01:09:13 +0700

    t10

--- .vercel/output/_functions/entry.mjs
+++ .vercel/output/_functions/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports } from './chunks/entrypoint_DbkRzT3A.mjs';
-import { manifest } from './manifest_Qbxcka7i.mjs';
+import { manifest } from './manifest_BxCpCGUu.mjs';
 
 const serverIslandMap = new Map([
 ]);;
@@ -25,7 +25,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "efe51b25-7228-490a-8f95-0f9b1531aad7",
+    "middlewareSecret": "6ad4fb24-bba5-4add-aa6e-c394abe2411b",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
